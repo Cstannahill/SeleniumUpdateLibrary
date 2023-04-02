fb65f4d8ffa489e0ddfeb8b93de3cd0519f55f87
Cstannahill <cstannahill10@gmail.com>
2023-04-01 21:04:37 -0500

    initial

---
+++ README.txt
@@ -0,0 +1,52 @@
+ -----------------FIRST BUILD THE LIBRARY-----------------------
+
+Download/clone this library somewhere on your pc.
+Open the library and build to generate bin/obj folders and create .dll
+Open the solution containing the project you are using Selenium for.
+
+
+
+
+
+-----------------TO ADD INTO EXISTING PROJECT--------------
+
+
+
+1. Right Click
+2.Add Project Reference 
+3. Browse
+4. Navigate to folder containing the class library you downloaded
+5.(Local Path to Where You Put Folder)\SeleniumUpdateLibrary\bin\Debug\netstandard2.0)
+6.Choose SeleniumUpdateLibrary.dll
+7. The project reference should show up now.
+
+
+--------------------TO USE---------------------------
+
+
+1. Add using statement at top level - using SeleniumUpdateLibrary;
+2. You will now be able to use methods and definitions in the class library within it's class of SeleniumHelper
+3. The main method you will want to use is - SeleniumHelper.UpdateDriver();
+(this will check your current version, the latest version, and if they don't match, it will delete current and download/install the latest)
+4. Include these within your code.
+
+SeleniumHelper.UpdateDriver();
+Console.WriteLine("Press Enter to exit...");
+Console.ReadLine();
+
+5. The reason for the ReadLine and WriteLine is for testing purposes, once it updates your driver, it will open a browser, writes some text into the search bar, and submits the search.
+This will effectively pause the program while you confirm it is working, and then you should be able to continue.
+
+
+
+
+------------------TO MODIFY OR REMOVE TEST-------------------------
+
+    Once you can confirm this program fixes your issue, you will most likely want to remove the test from it as it is quite intrusive.
+    
+    To do this --
+
+    1. Open class library project solution, In the SeleniumHelper.cs Class - comment or remove line 138 - await TestSel();
+    2. Save, build or rebuild.
+    Now you can include SeleniumHelper.UpdateDriver() to any project you use Selenium with and it can check driver version and update if necessary
+    This will no longer bring up the browser, but will still show the console output of driver versioning.
---
+++ SeleniumHelper.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumUpdateLibrary
+{
+    public class SeleniumHelper
+    {
+        public static async Task<string> UpdateChromeDriverIfNeeded()
+        {
+            string versionInfo;
+            bool isSuccessful = false;
+            string currentVersion = await GetCurrentVersionAsync();
+            Console.WriteLine($"{currentVersion} is your current version of chromeDriver.");
+            string latestVersion = await GetLatestVersionAsync();
+            Console.WriteLine($"{latestVersion} is the latest version of chromeDriver.");
+            if (currentVersion != latestVersion)
+            {
+                try
+                {
+                    SeleniumHelper manager = new SeleniumHelper();
+                    bool latest = await manager.DownloadLatestVersion(latestVersion);
+
+                    isSuccessful = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your Driver is up to date.");
+            }
+            versionInfo = $"Current Version: {currentVersion}; Latest Version: {latestVersion}; Your updated was successful: {isSuccessful}";
+            return versionInfo;
+        }
+
+        public static async Task<string> GetCurrentVersionAsync()
+        {
+            string driverPath = GetDriverPath();
+            ProcessStartInfo psi = new ProcessStartInfo(driverPath)
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                Arguments = "--version"
+            };
+
+            Process process = new Process { StartInfo = psi };
+            process.Start();
+
+            string output = await process.StandardOutput.ReadToEndAsync();
+            process.WaitForExit();
+
+            string version = output.Split(' ')[1].Trim();
+            return version;
+        }
+
+        private static async Task<string> GetLatestVersionAsync()
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = await httpClient.GetAsync("https://chromedriver.storage.googleapis.com/LATEST_RELEASE");
+                response.EnsureSuccessStatusCode();
+                string latestVersion = await response.Content.ReadAsStringAsync();
+                return latestVersion;
+            }
+        }
+
+        private async Task<bool> DownloadLatestVersion(string latestVersion)
+        {
+            string url = $"https://chromedriver.storage.googleapis.com/{latestVersion}/chromedriver_win32.zip";
+            string tempPath = Path.GetTempPath();
+            string zipPath = Path.Combine(tempPath, "chromedriver_win32.zip");
+            string extractPath = Path.Combine(tempPath, "chromedriver_win32");
+            string newDriverPath = Path.Combine(extractPath, "chromedriver.exe");
+            string currentDriverPath = GetDriverPath();
+
+            // Download the latest ChromeDriver ZIP
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(url);
+                using (var fileStream = new FileStream(zipPath, FileMode.Create, FileAccess.Write))
+                {
+                    await response.Content.CopyToAsync(fileStream);
+                }
+            }
+
+            // Extract the ZIP
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+            // Delete the old ChromeDriver if it exists
+            if (File.Exists(currentDriverPath))
+            {
+                File.Delete(currentDriverPath);
+            }
+
+            // Move the new ChromeDriver to the desired location
+            File.Move(newDriverPath, currentDriverPath);
+
+            // Cleanup: Delete the downloaded ZIP and extracted folder
+            File.Delete(zipPath);
+            Directory.Delete(extractPath, true);
+
+            return File.Exists(currentDriverPath);
+        }
+
+        private static string GetDriverPath()
+        {
+            string currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string driverPath = Path.Combine(currentPath, "chromedriver.exe");
+            return driverPath;
+        }
+        public async static void UpdateDriver()
+        {
+            try
+
+            {
+                string updateResult = Task.Run(() => SeleniumHelper.UpdateChromeDriverIfNeeded()).Result;
+                string updatedVersion = Task.Run(() => SeleniumHelper.GetCurrentVersionAsync()).Result;
+                Console.WriteLine($"Current Version after update is {updatedVersion}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //try to pop the browser using selenium to verify the update fixed the issue
+            await TestSel();
+
+        }
+
+        private static async Task TestSel()
+        {
+            using (ChromeDriver driver = new ChromeDriver())
+            {
+
+                driver.Navigate().GoToUrl("https://www.google.com");
+                await Task.Delay(3000);
+                IWebElement searchBox = driver.FindElement(By.Name("q")); 
+                await Task.Delay(1000);
+                searchBox.SendKeys("Your Selenium Seems to be working. Cool. Cool Cool Cool");
+                await Task.Delay(2000);
+                searchBox.Submit();
+                await Task.Delay(1000);
+                driver.Quit();
+            }
+        }
+    }
+}
